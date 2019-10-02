8d362df10c6a564f70921808cb1a9a9afde7a785
jaldazabal <jon3aldazabal@gmail.com>
2019-10-02 17:16:12 +0200

    Minor changes to object form

--- src/app/form-data.ts
+++ src/app/form-data.ts
@@ -1,3 +1,20 @@
 export class FormData {
+  public municipi: string;
+  public nom: string;
+  public cognoms: string;
+  public cip: string;
+  public nccr: string;
+  public email: string;
+  public phone: string;
+  public motiu: string;
+  // Carta perduda
+  public mediaRecieved: string;
+  // Ultims 5 anys
+  public anyExploracio: string;
+  public seguimentProgramat: string;
+  public centre: string;
+  public motiuUltimaExploracio: string;
+  public fitxersColono = [];
+
 
 }
