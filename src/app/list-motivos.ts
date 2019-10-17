364923121fcd6acf94d83678177132c72e8fdaf2
jaldazabal <jon3aldazabal@gmail.com>
2019-10-17 17:20:47 +0200

    Typo in motivos

--- src/app/list-motivos.ts
+++ src/app/list-motivos.ts
@@ -5,7 +5,7 @@ export const MOTIVOS: Motivo[] = [
   { id: 2, text: 'No he rebut la carta', step: 4 },
   { id: 3, text: 'He perdut la carta', step: 5 },
   { id: 4, text: 'He perdut la carta del resultat', step: 5 },
-  { id: 5, text: 'Tinc realitzada una colonòscopia als últims 5 anys', step: 6 },
+  { id: 5, text: 'Tinc realitzada una colonòscopia els últims 5 anys', step: 6 },
   { id: 6, text: 'Estic diagnosticat de pòlips o adenomes', step: 6 },
   { id: 7, text: 'Estic diagosticat de colitis ulcerosa o malaltia de Crohn', step: 7 },
   { id: 8, text: 'Estic diagnosticat de càncer de còlon i/o recte', step: 7 },
